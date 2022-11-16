eb176b86a4fb99afcb76fd009216946fc06d67cb
NunkyIroh <vadegtyarev@gmail.com>
2022-11-16 16:05:37 +0300

    Add new function

--- bot_commands.py
+++ bot_commands.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
 
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -10,3 +10,13 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Hello here! I'm a bot, please talk to me! I know some commands! Please, send '/Help' to me and you see this commands!"
+    )
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 app = ApplicationBuilder().token("5767897712:AAEnsMEY9J3eqyzVicNPIHf7x5O0XuPjQeg").build()
+
+start_handler = CommandHandler('Start', start)
+app.add_handler(start_handler)
 
 app.add_handler(CommandHandler("Hi", hi_command))
 app.add_handler(CommandHandler("Help", help_command))
 app.add_handler(CommandHandler("Time", time_command))
+
+echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    
+app.add_handler(start_handler)
+app.add_handler(echo_handler)
 
 
 
